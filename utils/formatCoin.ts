2e136d85876883ff2a1427efe098210e898e751b
Vinicius Celestino de Oliveira Pereira <vinigtr386@gmail.com>
2023-02-23 11:36:36 -0300

    Create formatCoin.ts

---
+++ utils/formatCoin.ts
@@ -0,0 +1,11 @@
+//On react native need to install intl and use -----> {
+//INTL
+import 'intl'
+import 'intl/locale-data/jsonp/pt-BR'
+} on the App.tsx
+
+export function formatCoin(coin: number) {
+  return new Intl.NumberFormat('pt-br',
+    { style:'currency', currency:'BRL'})
+    .format(coin)
+}
