72b230a5c0a02524d83168d3542711f2fdcb91f0
Vinicius Celestino de Oliveira Pereira <vinigtr386@gmail.com>
2023-02-23 11:19:09 -0300

    Update Text.ts

--- components/native/Text.ts
+++ components/native/Text.ts
@@ -11,3 +11,10 @@ export const Text = styled.Text<TextProps>`
   color: ${({ color, theme }) => color || theme.COLORS.WHITE};
   font-size: ${({ size }) => size ? `${size}px` : '16px'};
 `
+//Change the name of the font using this on the App.tsx
+const [fontsLoaded] = useFonts({
+    Inter_300: Inter_300Light,
+    Inter_400: Inter_400Regular,
+    Inter_500: Inter_500Medium,
+    Inter_700: Inter_700Bold,
+  })
