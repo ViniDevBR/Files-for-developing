bc657b4a8f2fa1e27533ebbb2a03823adf054204
Vinicius Celestino de Oliveira Pereira <vinigtr386@gmail.com>
2023-04-04 10:10:39 -0300

    Create modal.ts

---
+++ @types/modal.ts
@@ -0,0 +1,6 @@
+export interface IModal {
+  visible: boolean
+  onClose: VoidFunction
+}
+
+//Interface para extender usar e nao ficar copiando e colando, reaproveitando codigo e caso queira outras props, basta extender essa
