e31538d8eb0c454dc9906381a4017326828bb001
Vinicius Celestino de Oliveira Pereira <vinigtr386@gmail.com>
2023-10-18 11:16:23 -0300

    Update Text.ts

--- components/native/Text.ts
+++ components/native/Text.ts
@@ -1,16 +1,24 @@
-import styled, { DefaultTheme } from 'styled-components/native'
+import { styled, DefaultTheme } from 'styled-components/native'
 
 interface TextProps {
   weight?:  keyof DefaultTheme['WEIGHT']
   color?: keyof DefaultTheme['COLORS']
+  hexColor?: string
   size?: number
 }
 
 export const Text = styled.Text<TextProps>`
   font-family: ${({ theme, weight }) => weight ? `Inter_${theme.WEIGHT[weight]}` : 'Inter_400'};
-  color: ${({ theme, color }) => color ? theme.COLORS[color] : theme.COLORS.TEXT};
+  color: ${({ theme, color, hexColor }) =>
+    color ?
+      theme.COLORS[color]
+      :
+      hexColor ?
+        hexColor
+        :
+        theme.COLORS.TEXT};
   font-size: ${({ size }) => size ? `${size}px` : '16px'};
-`
+  
 
 //Change the name of the font using this on the App.tsx
 const [fontsLoaded] = useFonts({
