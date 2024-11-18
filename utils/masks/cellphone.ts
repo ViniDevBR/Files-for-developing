e68b0a8d3f8be2079f57e43f9288deee371d9791
Vinicius Celestino de Oliveira Pereira <vinigtr386@gmail.com>
2024-11-18 11:45:16 -0300

    Update cellphone.ts

--- utils/masks/cellphone.ts
+++ utils/masks/cellphone.ts
@@ -1,7 +1,39 @@
-export function cellPhoneMask(value: string) {
-  const regex = /^(\d{2})(\d{5})(\d{4})$/
-  const result = '($1) $2-$3'
+export function cellphoneMask(value: string) {
+  const onlyNumbers = value.replace(/\D/g, '')
 
-  return value.replace(regex, result)
+  // console.log(onlyNumbers, onlyNumbers.length)
+  if (onlyNumbers.length === 3) {
+    const regex = /^(\d{2})(\d{1})$/
+    const pattern = '($1) $2'
+    
+    return onlyNumbers.replace(regex, pattern)
+  }
+  if (onlyNumbers.length === 8) {
+    const regex = /^(\d{2})(\d{5})(\d{1})$/
+    const pattern = '($1) $2-$3'
+    
+    return onlyNumbers.replace(regex, pattern)
+  }
+  // const regex = /^(\d{2})(\d{5})(\d{4})$/
+
+  return value
 }
 //(00) 00000-0000
+
+export function cellphoneMask2(value: string) {
+  const onlyNumbers = value.replace(/\D/g, '')
+  
+  const regex = /^(\d{2})(\d{2})(\d{5})(\d{4})$/
+  const pattern = '+$1 ($2) $3-$4'
+
+  return onlyNumbers.replace(regex, pattern)
+}
+
+export function cellphoneMask3(value: string) {
+  const onlyNumbers = value.replace(/\D/g, '')
+  const regex = /^(\S{3})(\d{2})(\d{5})(\d{4})$/
+  const pattern = '$1 ($2) $3-$4'
+
+  return onlyNumbers.replace(regex, pattern)
+}
+// +55 (19) 98310-0871
