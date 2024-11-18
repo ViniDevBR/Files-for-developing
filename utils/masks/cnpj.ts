c41ab6de935c1ec65b7a91a83dbaea3f0296b6bd
Vinicius Celestino de Oliveira Pereira <vinigtr386@gmail.com>
2024-11-18 11:45:56 -0300

    Update cnpj.ts

--- utils/masks/cnpj.ts
+++ utils/masks/cnpj.ts
@@ -1,7 +1,36 @@
 export function cnpjMask(value: string) {
-  const regex = /^(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})$/
-  const result = '$1.$2.$3/$4-$5'
+  const onlyNumbers = value.replace(/\D/g, '')
 
-  return value.replace(regex, result)
+  // console.log(onlyNumbers, onlyNumbers.length)
+  
+  if (onlyNumbers.length === 3) {
+    const regex = /^(\d{2})(\d{1})$/
+    const result = '$1.$2'
+
+    return onlyNumbers.replace(regex, result)
+  }
+  if (onlyNumbers.length === 6) {
+    const regex = /^(\d{2})(\d{3})(\d{1})$/
+    const result = '$1.$2.$3'
+
+    return onlyNumbers.replace(regex, result)
+  }
+  if (onlyNumbers.length === 9) {
+    const regex = /^(\d{2})(\d{3})(\d{3})(\d{1})$/
+    const result = '$1.$2.$3/$4'
+
+    return onlyNumbers.replace(regex, result)
+  }
+  if (onlyNumbers.length === 13) {
+    const regex = /^(\d{2})(\d{3})(\d{3})(\d{4})(\d{1})$/
+    const result = '$1.$2.$3/$4-$5'
+
+    return onlyNumbers.replace(regex, result)
+  }
+  // const regex = /^(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})$/
+  // const result = '$1.$2.$3/$4-$5'
+
+  return value
 }
+//12345678900000
 //00.000.000/0000-00
