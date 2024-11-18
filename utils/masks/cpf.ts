6e7e15402a34403f9b712dd5c832833671ddb7a3
Vinicius Celestino de Oliveira Pereira <vinigtr386@gmail.com>
2024-11-18 11:46:14 -0300

    Update cpf.ts

--- utils/masks/cpf.ts
+++ utils/masks/cpf.ts
@@ -1,7 +1,30 @@
 export function cpfMask(value: string) {
-  const regex = /^(\d{3})(\d{3})(\d{3})(\d{2})$/
-  const result = '$1.$2.$3-$4'
+  const onlyNumbers = value.replace(/\D/g, '')
 
-  return value.replace(regex, result)
+  // console.log(onlyNumbers, onlyNumbers.length)
+
+  if (onlyNumbers.length === 4) {
+    const regex = /^(\d{3})(\d{1})$/
+    const result = '$1.$2'
+
+    return onlyNumbers.replace(regex, result)
+  }
+  if (onlyNumbers.length === 7) {
+    const regex = /^(\d{3})(\d{3})(\d{1})$/
+    const result = '$1.$2.$3'
+
+    return onlyNumbers.replace(regex, result)
+  }
+  if (onlyNumbers.length === 10) {
+    const regex = /^(\d{3})(\d{3})(\d{3})(\d{1})$/
+    const result = '$1.$2.$3-$4'
+
+    return onlyNumbers.replace(regex, result)
+  }
+  // const regex = /^(\d{3})(\d{3})(\d{3})(\d{2})$/
+  // const result = '$1.$2.$3-$4'
+
+  return value
 }
+//12345678900
 //000.000.000-00
