b2edc994c79044fbc60135b89b9ff40aac630e6d
Vinicius Celestino de Oliveira Pereira <vinigtr386@gmail.com>
2024-11-26 17:05:15 -0300

    Update dateWithoutTimezone.ts

--- utils/dateWithoutTimezone.ts
+++ utils/dateWithoutTimezone.ts
@@ -1,7 +1,9 @@
 export const dateWithoutTimezone = (date: Date) => {
-  const tzoffset = date.getTimezoneOffset() * 60000; //offset in milliseconds
+  const ONE_MINUTE_TO_MILISECONDS = 60000
+  const tzoffset = date.getTimezoneOffset() * ONE_MINUTE_TO_MILISECONDS;
+  // converte a diferença de fuso horário de minutos para milissegundos (1 minuto = 60.000 milissegundos).
   const withoutTimezone = new Date(date.valueOf() - tzoffset)
     .toISOString()
-    .slice(0, -1);
+    .slice(0, -1); // REMOVE O Z do final
   return withoutTimezone;
 };
