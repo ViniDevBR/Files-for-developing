48a9b62a1e7709e1ef866f6c8fc68843d45b68b2
Vinicius Celestino de Oliveira Pereira <vinigtr386@gmail.com>
2025-02-18 12:37:10 -0300

    Update getBase64.ts

--- utils/getBase64.ts
+++ utils/getBase64.ts
@@ -2,5 +2,9 @@ import * as FileSystem from 'expo-file-system'
 // ALWAYS CHECK EXPO DOCS TO SEE IF THERE IS SOME CHANGES
 
 export async function getBase64(fileUri: string, options?: FileSystem.ReadingOptions) {
+  if (!fileUri || fileUri === null || fileUri.length === 0) {
+    return ''
+  }
+  
   return await FileSystem.readAsStringAsync(fileUri, options || { encoding: 'base64' })
 }
