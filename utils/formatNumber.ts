5d6ca5807da7ccda2aff7e568f5cfaa5ea2ccbcd
Vinicius Celestino de Oliveira Pereira <vinigtr386@gmail.com>
2025-04-02 13:44:59 -0300

    Update formatNumber.ts

--- utils/formatNumber.ts
+++ utils/formatNumber.ts
@@ -17,6 +17,22 @@ export function formatShortenNumber(number: number) {
   return numeral(number).format('0.00a').replace('.00', '')
 }
 
+export function formatDecimal(
+  value?: number,
+  options?: Intl.NumberFormatOptions,
+  defaultValue?: string
+) {
+  if (typeof value !== 'number' || isNaN(value)) return defaultValue ?? '0,00'
+
+  return value.toLocaleString(
+    'pt-BR',
+    options ?? {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 4
+    }
+  )
+}
+
 // export function formatData(number: number) {
 //   return numeral(number).format('0.0 b')
 // }
